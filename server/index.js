94a89b8987ac87055a7caf9ee9183dfcbaa3bed8
Jakub Plecháč <loxtox74@gmail.com>
2022-10-02 19:18:30 +0200

    MongoDB set up

--- server/index.js
+++ server/index.js
@@ -2,3 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.unsubscribe(cors());
+
+const USERNAME = "jakub";
+const PASSWORD = "user321123";
+
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.ldxosag.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//mongoose.set("useFindAndModify", false);
