a5295d004be085967fefea9888d8b458cf07d873
Jakub Plecháč <loxtox74@gmail.com>
2022-10-06 18:23:34 +0200

    Edited Form, Posts and Postfile

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,36 @@ import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 import memories from "./images/memories.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
-    <div>
-      <Container>
-        <AppBar position="static" color="inherit">
-          <Typography variant="h2" align="center">
-            Memories
-            <img src={memories} alt="memories" height="60" />
-          </Typography>
-        </AppBar>
-        <Grow in>
-          <Container>
-            <Grid
-              container
-              justify="space-between"
-              alignItems="stretch"
-              spacing="3"
-            >
-              <Grid item xs="12" sm="7">
-                <Posts />
-              </Grid>
-              <Grid item xs="12" sm="7">
-                <Form />
-              </Grid>
+    <Container>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+          <img src={memories} alt="memories" height="60" />
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing="3"
+          >
+            <Grid item xs="12" sm="7">
+              <Posts />
             </Grid>
-          </Container>
-        </Grow>
-      </Container>
-    </div>
+            <Grid item xs="12" sm="7">
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
