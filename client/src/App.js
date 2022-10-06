9822d13b2e301075c812a0d56033ac86b84cdd88
Jakub Plecháč <loxtox74@gmail.com>
2022-10-06 18:19:20 +0200

    Edited App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import memories from "./images/memories.png";
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <Container>
+        <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Memories
+            <img src={memories} alt="memories" height="60" />
+          </Typography>
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justify="space-between"
+              alignItems="stretch"
+              spacing="3"
+            >
+              <Grid item xs="12" sm="7">
+                <Posts />
+              </Grid>
+              <Grid item xs="12" sm="7">
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
