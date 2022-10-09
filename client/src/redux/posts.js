cf39ad7258e711f84b894af9c7c8a54db0bd62bd
Jakub Plecháč <loxtox74@gmail.com>
2022-10-09 18:12:23 +0200

    Create addPost action

--- client/src/redux/posts.js
+++ client/src/redux/posts.js
@@ -11,6 +11,16 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   }
 });
 
+export const addPost = createAsyncThunk("posts/addPost", async (post) => {
+  try {
+    const { data } = await api.createPost(post);
+
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 export const postSlice = createSlice({
   name: "posts",
   initialState: {
@@ -18,35 +28,16 @@ export const postSlice = createSlice({
     status: "idle",
     error: null,
   },
-  reducers: {
-    addPost: {
-      reducer: (state, action) => {
-        state.posts.push(action.payload);
-      },
-      prepare(value) {
-        return {
-          payload: {
-            key: nanoid(),
-            value: value,
-          },
-        };
-      },
-    },
-  },
   extraReducers: {
-    [fetchPosts.pending]: (state, action) => {
-      state.status = "loading";
-    },
     [fetchPosts.fulfilled]: (state, action) => {
       state.status = "succeeded";
       state.posts.push(...action.payload);
     },
-    [fetchPosts.rejected]: (state, action) => {
-      state.status = "failed";
-      state.error = action.error.message;
+    [addPost.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state.posts.push(action.payload);
     },
   },
 });
 
-export const { addPost } = postSlice.actions;
 export default postSlice.reducer;
