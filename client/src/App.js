f4d962c1b6b388136e9b3e622cbaba224091ffc1
Jakub Plecháč <loxtox74@gmail.com>
2022-10-15 18:58:56 +0200

    Implemented React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { Route, Routes, Navigate } from "react-router-dom";
 
-import useStyles from "./styles";
-import memories from "./images/memories.png";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
+import Home from "./components/Home/Home";
 
 function App() {
-  const classes = useStyles();
-
   return (
-    <Container>
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-          <img
-            className={classes.image}
-            src={memories}
-            alt="memories"
-            height="60"
-          />
-        </Typography>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            className={classes.mainContainer}
-            container
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid className={classes.postsContainer} item md={12} lg={7}>
-              <Posts />
-            </Grid>
-            <Grid className={classes.gridContainer} item md={12} lg={4}>
-              <Form />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+    <Container maxWidth="xl">
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
     </Container>
   );
 }
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import useStyles from "../Home/styles";
+import memories from "../../images/memories.png";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <Grow in>
+      <Container>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            Memories
+            <img
+              className={classes.image}
+              src={memories}
+              alt="memories"
+              height="60"
+            />
+          </Typography>
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              className={classes.mainContainer}
+              container
+              justifyContent="space-between"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid className={classes.postsContainer} item md={12} lg={7}>
+                <Posts />
+              </Grid>
+              <Grid className={classes.gridContainer} item md={12} lg={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
